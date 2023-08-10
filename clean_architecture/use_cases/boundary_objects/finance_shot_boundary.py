78669156d237b5f12807b4b8957ed917cbb79af5
Jonathan <jonathangagnon@gmail.com>
2023-08-10 11:48:38 +0200

    adding the possibility to switch between views in flas

--- clean_architecture/use_cases/boundary_objects/finance_shot_boundary.py
+++ clean_architecture/use_cases/boundary_objects/finance_shot_boundary.py
@@ -11,13 +11,5 @@ class FinanceShotBoundary(object):
         self.cost = int()
         self.budget = int()
         self.title = 'no_title'
-        self._title_is_correct = True
 
-    @property
-    def title_is_correct(self):
-        return self._title_is_correct
-
-    @title_is_correct.setter
-    def title_is_correct(self, value):
-        self._title_is_correct = value
 
